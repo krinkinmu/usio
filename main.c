288ce01100e9f80410fd5d16fc04a6bbb57cd849
Mike Krinkin <krinkin.m.u@gmail.com>
2015-10-12 16:44:47 +0300

    updated version, not tested

--- main.c
+++ main.c
@@ -6,101 +6,305 @@
 #include <linux/pagemap.h>
 
 #include <linux/miscdevice.h>
+#include <linux/spinlock.h>
 #include <linux/uaccess.h>
+#include <linux/blkdev.h>
 #include <linux/file.h>
 #include <linux/bio.h>
 #include <linux/fs.h>
 
 #include "usio.h"
 
-static long usio_submit(struct usio_io *io)
-{
-	struct inode *inode;
-	struct block_device *bdev;
-	struct file *filep;
+struct usio_context {
+	spinlock_t lock;
+	struct list_head finished_list;
+	size_t finished_list_size;
+	struct list_head running_list;
+	wait_queue_head_t finished_wq;
+};
+
+struct usio_io_handle {
+	struct list_head link;
 	struct bio *bio;
+	struct usio_event event;
+	struct usio_context *ctx;
+	struct work_struct complete_work;
+};
+
+static struct kmem_cache *usio_io_handle_cachep;
+
+
+static void usio_bio_release_pages(struct bio *bio)
+{
+	struct bio_vec *bvec;
+	unsigned i;
+
+	bio_for_each_segment_all(bvec, bio, i)
+		page_cache_release(bvec->bv_page);
+}
+
+static void usio_end_io(struct bio *bio)
+{
+	struct usio_io_handle *handle = bio->bi_private;
+	struct usio_context *ctx = handle->ctx;
+
+	handle->event.res = bio->bi_error;
+	usio_bio_release_pages(bio);
+	bio_put(bio);
+
+	spin_lock(&ctx->lock);
+	++ctx->finished_list_size;
+	list_move(&handle->link, &ctx->finished_list);
+	spin_unlock(&ctx->lock);
+	wake_up_interruptible(&ctx->finished_wq);
+}
+
+static struct bio *usio_bio_create(struct usio_io *io,
+			struct block_device *bdev)
+{
 	struct page **pages;
-	int err = 0, npages, i;
+	struct bio *bio;
+	int npages = 0;
+	int offset, len;
+	int rc, i;
 
-	if ((io->io_buffer & PAGE_MASK) != io->io_buffer)
-		return -EINVAL;
+	if (io->data & ~PAGE_MASK)
+		++npages;
 
-	if ((io->io_nbytes & PAGE_MASK) != io->io_nbytes)
-		return -EINVAL;
+	npages += (io->bytes + PAGE_SIZE - 1) >> PAGE_SHIFT;
+	if (npages > BIO_MAX_PAGES)
+		return ERR_PTR(-EINVAL);
 
-	if (io->io_offset & ((1ul << 9) - 1))
-		return -EINVAL;
-
-	npages = io->io_nbytes >> PAGE_SHIFT;
-	pages = kmalloc(npages * sizeof(struct page *), GFP_KERNEL);
-	if (!pages)
-		return -ENOMEM;
-
-	err = get_user_pages_fast(io->io_buffer, npages, 0, pages);
-	if (err < 0)
-		goto free_pages;
-
-	if (err != npages) {
-		for (i = 0; i != err; ++i)
-			page_cache_release(pages[i]);
-		err = -ENOMEM;
-		goto free_pages;
-	}
-
-	filep = fget(io->io_filed);
-	if (!filep) {
-		err = -EBADF;
-		goto release_pages;
-	}
-
-	if (!filep->f_mapping) {
-		err = -EINVAL;
-		goto put_file;
-	}
-
-	inode = filep->f_mapping->host;
-	if (!inode || !(inode->i_mode & S_IFBLK)) {
-		err = -EINVAL;
-		goto put_file;
-	}
-
-	bdev = I_BDEV(inode);
-	pr_err("bdev pointer is %lx\n", (unsigned long)bdev);
-	pr_err("bdev->bd_inode is %lx\n", (unsigned long)bdev->bd_inode);
-	bio = bio_alloc(GFP_NOIO, BIO_MAX_PAGES);
-	if (!bio) {
-		err = -ENOMEM;
-		goto put_file;
-	}
-
+	bio = bio_alloc(GFP_KERNEL, npages);
 	bio->bi_bdev = bdev;
-	bio->bi_iter.bi_sector = io->io_offset >> 9;
+	bio->bi_iter.bi_sector = io->offset >> 9;
+	bio->bi_end_io = usio_end_io;
 
+	pages = kcalloc(npages, sizeof(*pages), GFP_KERNEL);
+	if (!pages) {
+		bio_put(bio);
+		return ERR_PTR(-ENOMEM);
+	}
+
+	rc = get_user_pages_fast(io->data, npages,
+				(io->flags & REQ_WRITE) != REQ_WRITE, pages);
+	if (rc < npages) {
+		rc = -EFAULT;
+		goto out;
+	}
+
+	rc = 0;
+	offset = io->data & ~PAGE_MASK;
+	len = io->bytes;
 	for (i = 0; i != npages; ++i) {
-		if (bio_add_page(bio, pages[i], PAGE_SIZE, 0) != PAGE_SIZE) {
-			err = -EIO;
-			goto release_bio;
+		unsigned bytes = PAGE_SIZE - offset;
+
+		if (bytes > len)
+			bytes = len;
+
+		if (bio_add_page(bio, pages[i], bytes, offset) != bytes) {
+			rc = -EFAULT;
+			goto out;
+		}
+
+		offset = 0;
+		len -= bytes;
+	}
+
+out:
+	if (rc) {
+		bio_put(bio);
+		for (i = 0; i != npages; ++i) {
+			if (!pages[i])
+				break;
+			page_cache_release(pages[i]);
 		}
 	}
 
-	bio_get(bio);
-	submit_bio_wait(REQ_WRITE, bio);
-
-release_bio:
-	bio_put(bio);
-put_file:
-	fput(filep);
-release_pages:
-	for (i = 0; i != npages; ++i)
-		page_cache_release(pages[i]);
-free_pages:
 	kfree(pages);
-	return err;
+	return rc ? ERR_PTR(rc) : bio;
+}
+
+static long usio_submit_one(struct usio_context *ctx,
+			struct usio_io __user *uio, struct usio_io *io)
+{
+	struct file *filep = fget(io->fd);
+	struct block_device *bdev;
+	struct usio_io_handle *handle;
+	struct inode *inode;
+	struct bio *bio;
+	unsigned blksize_mask;
+	unsigned long flags;
+
+	if (!filep || !filep->f_mapping)
+		return -EBADF;
+
+	inode = filep->f_mapping->host;
+	if (!inode || !(inode->i_mode & S_IFBLK)) {
+		fput(filep);
+		return -EINVAL;
+	}
+
+	bdev = I_BDEV(inode);
+	blksize_mask = (1 << blksize_bits(bdev_logical_block_size(bdev))) - 1;
+
+	if (io->offset & blksize_mask) {
+		fput(filep);
+		return -EINVAL;
+	}
+
+	handle = kmem_cache_alloc(usio_io_handle_cachep, GFP_KERNEL);
+	if (!handle) {
+		fput(filep);
+		return -ENOMEM;
+	}
+
+	bio = usio_bio_create(io, bdev);
+	if (IS_ERR(bio)) {
+		kmem_cache_free(usio_io_handle_cachep, handle);
+		fput(filep);
+		return PTR_ERR(bio);
+	}
+
+	bio->bi_private = handle;
+	handle->ctx = ctx;
+	handle->event.io = (__u64)uio;
+	handle->event.res = 0;
+	handle->bio = bio;
+
+	spin_lock_irqsave(&ctx->lock, flags);
+	list_add_tail(&handle->link, &ctx->running_list);
+	spin_unlock_irqrestore(&ctx->lock, flags);
+
+	submit_bio(io->flags, bio);
+	fput(filep);
+
+	return 0;
+}
+
+static long usio_submit_all(struct usio_context *ctx,
+			struct usio_ios __user *ios)
+{
+	struct usio_ios copy;
+	struct blk_plug plug;
+
+	struct usio_io __user *__user *iopp;
+	long count, i, rc;
+
+	if (copy_from_user(&copy, ios, sizeof(copy)))
+		return -EFAULT;
+
+	iopp = copy.ios;
+	count = copy.count;
+
+	if (!access_ok(VERIFY_READ, iopp, count * sizeof(*iopp)))
+		return -EFAULT;
+
+	blk_start_plug(&plug);
+	for (i = 0; i < count; ++i) {
+		struct usio_io __user *io;
+		struct usio_io tmp;
+
+		if (__get_user(io, iopp + i)) {
+			rc = -EFAULT;
+			break;
+		}
+
+		if (copy_from_user(&tmp, io, sizeof(tmp))) {
+			rc = -EFAULT;
+			break;
+		}
+
+		rc = usio_submit_one(ctx, io, &tmp);
+		if (rc)
+			break;
+	}
+	blk_finish_plug(&plug);
+
+	return i ? i : rc;
+}
+
+static size_t usio_ctx_finished(struct usio_context *ctx)
+{
+	unsigned long flags;
+	size_t ret;
+
+	spin_lock_irqsave(&ctx->lock, flags);
+	ret = ctx->finished_list_size;
+	spin_unlock_irqrestore(&ctx->lock, flags);
+
+	return ret;
+}
+
+static long usio_reclaim(struct usio_context *ctx,
+			struct usio_events __user *uevents)
+{
+	struct usio_events copy;
+	struct usio_event __user *events;
+	unsigned long flags;
+	long finished;
+	long min_count;
+	long max_count;
+	struct list_head splice;
+	struct list_head *pos, *tmp;
+	long rc = 0, i = 0;
+
+	if (copy_from_user(&copy, uevents, sizeof(copy)))
+		return -EFAULT;
+
+	events = copy.events;
+	min_count = copy.min_count;
+	max_count = copy.max_count;
+
+	if (!access_ok(VERIFY_WRITE, events, max_count * sizeof(*events)))
+		return -EFAULT;
+
+	wait_event_interruptible(ctx->finished_wq,
+				usio_ctx_finished(ctx) < min_count);
+
+	if (signal_pending(current))
+		return -EINTR;
+
+	INIT_LIST_HEAD(&splice);
+	spin_lock_irqsave(&ctx->lock, flags);
+	list_splice_init(&ctx->finished_list, &splice);
+	finished = ctx->finished_list_size;
+	ctx->finished_list_size = 0;
+	spin_unlock_irqrestore(&ctx->lock, flags);
+
+	list_for_each_safe(pos, tmp, &splice) {
+		struct usio_io_handle *handle = list_entry(pos,
+					struct usio_io_handle, link);
+		struct usio_event *event = &handle->event;
+
+		if (i == max_count)
+			break;
+
+		if (__copy_to_user(events + i, event, sizeof(*event))) {
+			rc = -EFAULT;
+			break;
+		}
+
+		list_del(pos);
+		kmem_cache_free(usio_io_handle_cachep, handle);
+		++i;
+	}
+
+	if (!list_empty(&splice)) {
+		spin_lock_irqsave(&ctx->lock, flags);
+		list_splice_tail(&splice, &ctx->finished_list);
+		ctx->finished_list_size += finished - i;
+		spin_unlock_irqrestore(&ctx->lock, flags);
+		wake_up_interruptible(&ctx->finished_wq);
+	}
+
+	return i ? i : rc;
 }
 
 static long usio_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 {
-	struct usio_io io;
+	struct usio_context *ctx = filep->private_data;
+	int rc = 0;
 
 	if (_IOC_TYPE(cmd) != USIO_IOC_MAGIC)
 		return -ENOTTY;
@@ -108,15 +312,59 @@ static long usio_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 	if (_IOC_NR(cmd) > USIO_IOC_MAXNR)
 		return -ENOTTY;
 
-	if (copy_from_user(&io, (void __user *)arg, sizeof(io)))
-		return -EFAULT;
+	switch (cmd) {
+	case USIO_SUBMIT:
+		rc = usio_submit_all(ctx, (struct usio_ios __user *)arg);
+		break;
+	case USIO_RECLAIM:
+		rc = usio_reclaim(ctx, (struct usio_events __user *)arg);
+		break;
+	default:
+		return -ENOTTY;
+	}
 
-	return usio_submit(&io);
+	return rc;
+}
+
+static int usio_open(struct inode *inode, struct file *file)
+{
+	struct usio_context *ctx = kmalloc(sizeof(*ctx), GFP_KERNEL);
+
+	if (!ctx) {
+		pr_err("Cannot allocate usio_context\n");
+		return -ENOMEM;
+	}
+
+	spin_lock_init(&ctx->lock);
+	INIT_LIST_HEAD(&ctx->finished_list);
+	ctx->finished_list_size = 0;
+	INIT_LIST_HEAD(&ctx->running_list);
+	init_waitqueue_head(&ctx->finished_wq);
+	file->private_data = ctx;
+
+	return 0;
+}
+
+static int usio_release(struct inode *inode, struct file *file)
+{
+	struct usio_context *ctx = (struct usio_context *)file->private_data;
+	struct list_head *pos, *tmp;
+
+	wait_event(ctx->finished_wq, !list_empty(&ctx->running_list));
+	list_for_each_safe(pos, tmp, &ctx->finished_list) {
+		struct usio_io_handle *handle = list_entry(pos,
+					struct usio_io_handle, link);
+		kmem_cache_free(usio_io_handle_cachep, handle);
+	}
+	kfree(ctx);
+	return 0;
 }
 
 static const struct file_operations usio_fops = {
 	.owner = THIS_MODULE,
 	.unlocked_ioctl = usio_ioctl,
+	.open = usio_open,
+	.release = usio_release,
 };
 
 static struct miscdevice usio_dev = {
@@ -128,10 +376,19 @@ static struct miscdevice usio_dev = {
 
 static int __init usio_init(void)
 {
-	const int rc = misc_register(&usio_dev);
+	int rc;
 
-	if (rc)
-		pr_debug("misc_register failed\n");
+	usio_io_handle_cachep = KMEM_CACHE(usio_io_handle, 0);
+	if (!usio_io_handle_cachep) {
+		pr_err("Cannot create usio_io_handle cache\n");
+		return -ENOMEM;
+	}
+
+	if ((rc = misc_register(&usio_dev))) {
+		kmem_cache_destroy(usio_io_handle_cachep);
+		pr_err("Cannot register misc device\n");
+		return rc;
+	}
 
 	return 0;
 }
@@ -139,6 +396,7 @@ static int __init usio_init(void)
 static void __exit usio_exit(void)
 {
 	misc_deregister(&usio_dev);
+	kmem_cache_destroy(usio_io_handle_cachep);
 }
 
 module_init(usio_init);
--- usio.h
+++ usio.h
@@ -5,16 +5,32 @@
 #include <linux/types.h>
 
 struct usio_io {
-	__u64 io_buffer;
-	__u64 io_nbytes;
-	__u64 io_offset;
-	__u32 io_flags;
-	__u32 io_filed;
+	__u64 data;
+	__u64 bytes;
+	__u64 offset;
+	__u32 flags;
+	__u32 fd;
+};
+
+struct usio_ios {
+	__u32 count;
+	struct usio_io __user *__user *ios;
+};
+
+struct usio_event {
+	__u64 io;
+	__s64 res;
+};
+
+struct usio_events {
+	__u32 min_count;
+	__u32 max_count;
+	struct usio_event __user *events;
 };
 
 #define USIO_IOC_MAGIC 'S'
-
-#define USIO_SUBMIT    _IOW(USIO_IOC_MAGIC, 1, struct usio_io)
-#define USIO_IOC_MAXNR 1
+#define USIO_SUBMIT  _IOW(USIO_IOC_MAGIC, 1, struct usio_ios)
+#define USIO_RECLAIM _IOR(USIO_IOC_MAGIC, 2, struct usio_events)
+#define USIO_IOC_MAXNR 2
 
 #endif /*__SUIO_H__*/
